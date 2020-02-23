555e8fc5a5f4f63f7d0eb58043611b9ca1f320e8
Nick Carson <nhc32@cam.ac.uk>
2020-02-23 15:45:39 +0000

    Update RecalculateView.js

--- src/RecalculateView.js
+++ src/RecalculateView.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-import SplitPane, { Pane } from 'react-split-pane';
-
-
-
 function RecalculateView() {
     return (
       <div style={{backgroundColor: "#E4E4E4",
@@ -12,13 +8,13 @@ function RecalculateView() {
                    alignItems: "center",
                    width: "100%",
                    height: "100%"}}>
-          <button onClick={CallAPI}><h2>Re-run the model</h2></button>
+          <button onClick={handleClick}><h2>Re-run the model</h2></button>
       </div>
     );
 }
 
-function CallAPI() {
-  alert("hello world");
+function handleClick() {
+  alert("Hello World");
   return;
 }
   
