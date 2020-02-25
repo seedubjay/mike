764af29c83193bf19d20c9c2415debf2b47ed36d
Isy Lynch <isylynch@hotmail.com>
2020-02-25 16:38:53 +0000

    changed control pannel size

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className={classes.root}>
       
-      <SplitPane split="vertical" defaultSize={620} primary="second">
+      <SplitPane split="vertical" defaultSize="27%" primary="second">
         <MapView detail={detail} setDetail={setDetail}/>
         <ControlView region={detail} setIsQuerying={setIsQuerying}/>
       </SplitPane>
